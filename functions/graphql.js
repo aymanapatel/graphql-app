9a6417da2d91e80bca7651cd2c0b7f0e15e3979e
Ayman Arif <ayman.patel97@gmail.com>
2020-08-16 11:41:32 +0530

    functionality: create <CreateJobListing> component

--- functions/graphql.js
+++ functions/graphql.js
@@ -1,80 +1,93 @@
-const { ApolloServer, gql} = require("apollo-server-lambda")
+const { ApolloServer, gql } = require("apollo-server-lambda");
 
 const typeDefs = gql`
-    type Query {
-        listings: [Listing!]!
-    }
-
-    type Listing {
-        id:ID!
-        title: String!
-        description: String
-        url: String!
-        company: Company
-        contacts: [Contact!]!
-    }
-    
-    type Contact {
-        id: ID!
-        name: String!
-        company: Company
-        email: String
-        note: String
-    }
-
-    type Company { 
-        id: ID!
-        name: String!
-        logo: String!
-        url: String
-        listings:[Listing!]!
-    }
+  type Query {
+    listings: [Listing!]!
+  }
+  type Mutation {
+    createListing(input: CreateListingInput!): Listing!
+  }
+  input CreateListingInput {
+    title: String!
+    description: String
+    url: String!
+    notes: String
+  }
+  type Contact {
+    id: ID!
+    name: String!
+    company: Company
+    email: String
+    notes: String
+  }
+  type Company {
+    id: ID!
+    name: String!
+    logo: String
+    listings: [Listing!]!
+    url: String
+  }
+  type Listing {
+    id: ID!
+    title: String!
+    description: String
+    url: String!
+    notes: String
+    company: Company
+    contacts: [Contact!]!
+  }
 `;
 
+const mockData = [
+  {
+    id: 1,
+    title: "Software Developer",
+    description:
+      "This candidate should have a strong grasp on developing software",
+    url: "https://myjobboard.com/acme/software-developer.php",
+    note: null,
+    company: {
+      id: 1,
+      name: "Acme Inc.",
+      url: "https://acme.com",
+      listings: [],
+    },
+    contacts: [],
+  },
+  {
+    id: 2,
+    title: "Developer Advocate",
+    description:
+      "This candidate should have a strong grasp on developer advocacy",
+    url: "https://myjobboard.com/acme/developer-advocate.php",
+    note: null,
+    company: {
+      id: 1,
+      name: "Acme Inc.",
+      url: "https://acme.com",
+      listings: [],
+    },
+    contacts: [],
+  },
+];
 
 const resolvers = {
-    Query: {
-        /*graphqlPlayground: (parent, args, contenxt) => {
-            return "Hello from the other side"
-        }*/
-        listings() {
-            return [
-                {
-                    id: 1,
-                    title: "Software Developer",
-                    description: "Google Cloud Platform",
-                    url: "www.google.com",
-                    note: null,
-                    company: {
-                        id: 121,
-                        name: "Google",
-                        url: "www.google.com",
-                        listings: null
-                    },
-                    contacts: []
-                },
-                {
-                    id: 2,
-                    title: "Software Advocate",
-                    description: "Hashicorp Multi-cloud",
-                    url: "www.hashicorp.com",
-                    note: null,
-                    company: {
-                        id: 121,
-                        name: "Hashicorp",
-                        url: "www.hashicorp.com",
-                        listings: null
-                    },
-                    contacts: []
-                }
-            ]
-        }
-    }
-}
+  Query: {
+    listings() {
+      return mockData;
+    },
+  },
+  Mutation: {
+    createListing(_, params, context) {
+      console.log(params);
+      return { ...mockData[0], ...params.input, id: 3 };
+    },
+  },
+};
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers
-})
+  typeDefs,
+  resolvers,
+});
 
 exports.handler = server.createHandler();
